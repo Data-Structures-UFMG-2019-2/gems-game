d69a55db45f723ce0e13280160d7862d5c0d420a
LuizPPA <luizphilippep@gmail.com>
2020-10-20 14:26:38 -0300

    Time test optimization

--- time.sh
+++ time.sh
@@ -3,11 +3,11 @@ TMP_OUT=$2
 INPUT_GENERATOR=$3
 
 for i in {1..255..1}; do
+    testname=$(printf "%03d_tmp" $i)
+    $INPUT_GENERATOR $i input/$testname.in
     for j in {1..10..1}; do
-        testname=$(printf "%03d_tmp" $i)
-        $INPUT_GENERATOR $i input/$testname.in
         $EXEC input/$testname.in timed > $TMP_OUT
-        rm input/$testname.in
     done
+    rm input/$testname.in
 done
 rm $TMP_OUT
